4440619f9d1498c4ca4b7f456ed36b9fed11cacd
agussatria <agussatria712@gmail.com>
2018-09-26 18:13:49 +0800

    Firts Commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        $faker = Faker\Factory::create();
+        $limit = 20;
+
+        for ($i = 0; $i < $limit; $i++){
+            DB::table('kontaks')->insert([
+                'nama'=> $faker->name,
+                'email'=> $faker->unique()->email,
+                'nohp'=> $faker->phoneNumber,
+                'alamat'=>$faker->address,
+            ]);
+        }
+    }
+}
